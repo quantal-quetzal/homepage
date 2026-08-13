43a07a379782d89ea04e9ab484fe93008f134770
quantal-quetzal <github@felix-gehring.de>
2026-08-13 10:42:59 +0200

    remove blog and migrate to vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ExperienceList from "./components/ExperienceList";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main className="mx-auto w-full max-w-[70rem] px-5 pb-10 pt-[7.2rem]">
+        <Profile />
+        <ExperienceList />
+      </main>
+    </>
+  );
+}
